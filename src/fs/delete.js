28b97db720397e5a82e643ede4dfdb1a9c2140c7
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-24 01:17:18 +0300

    feat: add fs delete, read, list subtasks

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await fs.unlink(filePath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const dirPath = path.join(import.meta.dirname, 'files');
+
 const list = async () => {
-  // Write your code here
+  try {
+    const files = await fs.readdir(dirPath);
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
