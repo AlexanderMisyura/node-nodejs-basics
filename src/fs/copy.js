fa4ea704afdbeb1b072121adca7ce2e5d8b23333
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-23 22:43:21 +0300

    feat: add fs copy subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const sourceDir = path.join(import.meta.dirname, 'files');
+const targetDir = path.join(import.meta.dirname, 'files_copy');
+
 const copy = async () => {
-  // Write your code here
+  try {
+    await fs.cp(sourceDir, targetDir, {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
