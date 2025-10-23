8e653456796ea9a6b616865b6313715a0204a785
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-24 00:38:23 +0300

    feat: add fs rename subtask

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const sourceFile = path.join(import.meta.dirname, 'files', 'wrongFilename.txt');
+const targetFile = path.join(import.meta.dirname, 'files', 'properFilename.md');
+
+const fsErrorMessage = 'FS operation failed';
+
 const rename = async () => {
-  // Write your code here
+  try {
+    await fs.access(targetFile);
+    throw new Error(fsErrorMessage);
+  } catch (error) {
+    if (error instanceof Error && error.message === fsErrorMessage) {
+      throw error;
+    }
+  }
+
+  try {
+    await fs.rename(sourceFile, targetFile);
+  } catch {
+    throw new Error(fsErrorMessage);
+  }
 };
 
 await rename();
