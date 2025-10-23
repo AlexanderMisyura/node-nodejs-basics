dafc44a7a9625263926e28e230661f1dc1087118
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-23 22:41:55 +0300

    feat: add fs create subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(import.meta.dirname, 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-  // Write your code here
+  try {
+    await fs.writeFile(filePath, fileContent, { flag: 'wx' });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
