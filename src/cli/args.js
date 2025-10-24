3e7d9a11d2cd1c89e5a741e4a37416a7d4e7eb4e
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-24 16:24:34 +0300

    feat: add cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+  let result = '';
+
+  process.argv.slice(2).forEach((arg, index) => {
+    if (index % 2 === 0) {
+      const name = arg.indexOf('--') === 0 ? arg.slice(2) : arg;
+      result += `${name} is `;
+    } else {
+      result += arg;
+      console.log(result);
+      result = '';
+    }
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
-const parseEnv = () => {
-  // Write your code here
-};
+const variablePrefix = 'RSS_';
+
+const parseEnv = () =>
+  Object.entries(process.env)
+    .filter(([key]) => key.startsWith(variablePrefix))
+    .forEach(([key, value]) => console.log(`${key}=${value}`));
 
 parseEnv();
