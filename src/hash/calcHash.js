15013b095fa4e36d2b63efeb1ef3751e777cd1f3
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-25 18:12:10 +0300

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import stream from 'stream/promises';
+
+const filePath = path.join(
+  import.meta.dirname,
+  'files',
+  'fileToCalculateHashFor.txt'
+);
+
 const calculateHash = async () => {
-  // Write your code here
+  const fileHash = crypto.createHash('sha256');
+  const readStream = fs.createReadStream(filePath);
+
+  await stream.pipeline(readStream, fileHash);
+  console.log(fileHash.digest('hex'));
 };
 
 await calculateHash();
