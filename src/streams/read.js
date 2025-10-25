54a9cc9d94cfb368411b7bac060f01dec527a598
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-25 18:13:38 +0300

    feat: add streams subtasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import stream from 'stream/promises';
+
+const sourceFile = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-  // Write your code here
+  const readStream = fs.createReadStream(sourceFile);
+  await stream.pipeline(readStream, process.stdout);
 };
 
 await read();
