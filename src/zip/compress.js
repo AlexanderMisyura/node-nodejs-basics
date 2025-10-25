fa71f067a2ba3b76e7993f0be6db47303bdf6afd
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-25 18:15:58 +0300

    feat: add zip subtasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import stream from 'stream/promises';
+
+const sourceFile = path.join(import.meta.dirname, 'files', 'fileToCompress.txt');
+const targetArchive = path.join(import.meta.dirname, 'files', 'archive.gz');
+
+
 const compress = async () => {
-  // Write your code here
+  const gzip = zlib.createGzip();
+  const readStream = fs.createReadStream(sourceFile);
+  const writeStream = fs.createWriteStream(targetArchive);
+
+  await stream.pipeline(readStream, gzip, writeStream);
 };
 
 await compress();
