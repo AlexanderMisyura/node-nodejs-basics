a500cfb31931a13a532793f08d53b2cf8689f0ef
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-25 18:26:33 +0300

    feat: add wt subtasks

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from 'worker_threads';
+
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  if (typeof workerData !== 'number' || workerData < 0) {
+    throw new Error('Incorrect data');
+  }
+
+  parentPort?.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
