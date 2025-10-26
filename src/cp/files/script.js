155047e809fe5a96b8addc9f8a4337d6aa7f1ab0
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-26 21:20:29 +0300

    feat: add cp subtask

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -6,6 +6,7 @@ const args = argv.slice(2);
 console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}${EOL}`);
 
+/** @param {Buffer} chunk */
 const echoInput = (chunk) => {
   const chunkStringified = chunk.toString();
 
