0f402b366b65502b0563777fc3a720a026bb28c9
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-10-26 21:10:24 +0300

    fix: fix successful copy with existent empty destination directory

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,15 +4,22 @@ import path from 'path';
 const sourceDir = path.join(import.meta.dirname, 'files');
 const targetDir = path.join(import.meta.dirname, 'files_copy');
 
+const fsErrorMessage = 'FS operation failed';
+
 const copy = async () => {
   try {
-    await fs.cp(sourceDir, targetDir, {
-      recursive: true,
-      force: false,
-      errorOnExist: true,
-    });
-  } catch {
-    throw new Error('FS operation failed');
+    await fs.access(targetDir);
+    throw new Error(fsErrorMessage);
+  } catch (error) {
+    if (error instanceof Error && error.message === fsErrorMessage) {
+      throw error;
+    }
+  }
+
+  try {
+    await fs.cp(sourceDir, targetDir, { recursive: true });
+  } catch (error) {
+    throw new Error(fsErrorMessage);
   }
 };
 
